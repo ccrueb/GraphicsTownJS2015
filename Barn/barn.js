55e2d07ee0cf4b2ed18949b0e77f4d24533a29bd
Cal Rueb <calvin.c.rueb@gmail.com>
2016-12-15 17:10:07 -0600

    roof on barn

--- Barn/barn.js
+++ Barn/barn.js
@@ -7,6 +7,7 @@ var Barn;
 (function() {
     "use strict";
     var shaderProgram = undefined;
+    var roof;
     var baseBuffers;
     var roofBuffers;
     var woodTexture;
@@ -25,17 +26,44 @@ var Barn;
         
         if (!shaderProgram) {
             shaderProgram = twgl.createProgramInfo(gl, ["barn-vs", "barn-fs"]);
+            roof = twgl.createProgramInfo(gl, ["cube-vs", "cube-fs"]);
         }
         //
         baseBuffers = twgl.primitives.createCubeBufferInfo(gl,this.size);
-        console.log(baseBuffers);
         woodTexture = twgl.createTexture(gl, { src: "Barn/textures/wood.jpg"})
         uwTexture = twgl.createTexture(gl, { src: "Barn/textures/uw.png"})
+        
+        //Roof
+        var arrays = {
+                vpos : { numComponents: 3, data: [
+                    
+                    -this.size/2,-this.size/2,-this.size/2,  this.size/2,-this.size/2,-this.size/2,  this.size/2,-this.size/2, this.size/2,        -this.size/2,-this.size/2,-this.size/2,  this.size/2,-this.size/2, this.size/2, -this.size/2,-this.size/2, this.size/2,    // y = 0
+                    -this.size/2, -this.size/2,-this.size/2,  this.size/2, -this.size/2,-this.size/2,  0, this.size/2,0,
+                     this.size/2, -this.size/2,-this.size/2,  this.size/2, -this.size/2, this.size/2,  0, this.size/2,0,  
+                     this.size/2, -this.size/2, this.size/2,  -this.size/2, -this.size/2, this.size/2,  0, this.size/2,0,  
+                    -this.size/2, -this.size/2, this.size/2,  -this.size/2, -this.size/2,-this.size/2,  0, this.size/2,0,  
+                      
+                ] },
+                vnormal : {numComponents:3, data: [
+                    0,0,-1, 0,0,-1, 0,0,-1,     0,0,-1, 0,0,-1, 0,0,-1,
+                    0,0,1, 0,0,1, 0,0,1,        0,0,1, 0,0,1, 0,0,1,
+                    0,-1,0, 0,-1,0, 0,-1,0,     0,-1,0, 0,-1,0, 0,-1,0,
+                    0,1,0, 0,1,0, 0,1,0,        0,1,0, 0,1,0, 0,1,0,
+                    -1,0,0, -1,0,0, -1,0,0,     -1,0,0, -1,0,0, -1,0,0,
+                    1,0,0, 1,0,0, 1,0,0,        1,0,0, 1,0,0, 1,0,0,
+                ]}
+        }
+            
+            roofBuffers = twgl.createBufferInfoFromArrays(drawingState.gl,arrays);
+            
+        
     };
     Barn.prototype.draw = function(drawingState) {
         var gl = drawingState.gl;
       
         var modelM = twgl.m4.identity();
+        twgl.m4.setTranslation(modelM,this.position, modelM)
+        twgl.m4.translate(modelM, [0, this.size/2,0], modelM)
         gl.useProgram(shaderProgram.program);
         twgl.setBuffersAndAttributes(gl, shaderProgram, baseBuffers);
         
@@ -50,7 +78,16 @@ var Barn;
         
         twgl.drawBufferInfo(gl, baseBuffers);
         
-        
+        //Roof
+        modelM = twgl.m4.identity();
+        twgl.m4.setTranslation(modelM,this.position, modelM)
+        twgl.m4.translate(modelM, [0, this.size*1.5,0], modelM)
+        gl.useProgram(roof.program);
+        twgl.setBuffersAndAttributes(gl, roof, roofBuffers);
+        twgl.setUniforms(roof,{
+            view:drawingState.view, proj:drawingState.proj, lightdir:drawingState.sunDirection,
+            cubecolor:[this.size/2,.2,.2], model: modelM });
+        twgl.drawBufferInfo(gl, roofBuffers);
 
     };
     Barn.prototype.center = function(drawingState) {
@@ -58,4 +95,4 @@ var Barn;
     }
 
 })();
-grobjects.push(new Barn("Barn1",[-2,1,   0],5) );
+grobjects.push(new Barn("Barn1",[3,0,   -3],4) );
